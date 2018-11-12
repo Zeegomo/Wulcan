d323dfd83013be42729cbf52c155bc813fcce12c
zeegomo <g.pasini98@gmail.com>
2018-11-12 12:02:51 +0100

    change lib location

--- src/wulcan/Test.java
+++ src/wulcan/Test.java
@@ -1,17 +1,4 @@
-package wulcan; 
-
-import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
-import static org.lwjgl.glfw.GLFW.*;
-import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.system.MemoryStack.stackPush;
-import static org.lwjgl.system.MemoryUtil.NULL;
-
-import java.nio.IntBuffer;
-
-import org.lwjgl.glfw.GLFWErrorCallback;
-import org.lwjgl.glfw.GLFWVidMode;
-import org.lwjgl.opengl.GL;
-import org.lwjgl.system.MemoryStack;
+package wulcan;
 
 public class Test {
 	
