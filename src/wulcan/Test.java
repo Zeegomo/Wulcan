9cf527302e8cb8900dfecacccbd5d09ade6c91a3
zeegomo <g.pasini98@gmail.com>
2018-11-13 17:57:08 +0100

    add lib to track

--- src/wulcan/Test.java
+++ src/wulcan/Test.java
@@ -1,15 +1,22 @@
 package wulcan;
 
+
 public class Test {
-	
+
+	static double[][] matrix = {
+			{0.9999	,0.0100495	,-0.0099495},
+			{-0.0099495	,0.9999	,0.0100495},
+			{0.0100495	,-0.0099495	,0.9999},
+			};
+
 	public static void main(String[] args) {
 		Point2D p1 = new Point2D(-1.0, 0.0);
 		Point2D p2 = new Point2D(0, 0);
 		Point2D p3 = new Point2D(-1, -1);
 		Color32 c = new Color32(1.0, 0.0, 1.0);
-		
+
 		double fov = 3.1415/2;
-		
+
 		Point3D[] points = new Point3D[8];
 		Point2D[] points_p = new Point2D[8];
 		points[0] = new Point3D(1,-1,2);
@@ -20,21 +27,22 @@ public class Test {
 		points[5] = new Point3D(1,1,4);
 		points[6] = new Point3D(-1,-1,4);
 		points[7] = new Point3D(-1,1,4);
-		
+
 		long time = System.nanoTime();
 		long fps = 0;
 		View2D view = new OpenGLView(1200, 800);
+
 		while(view.isAvailable()) {
 			for (int i = 0; i < 8; i++) {
+				rotate(points[i]);
 
 				points_p[i] = new Point2D(points[i].x, points[i].y);
 				points_p[i].x = points_p[i].x * view.getHeight() / view.getWidth();
-				points_p[i].x = points_p[i].x / (Math.tan(fov/2)*points[i].z);
-				points_p[i].y = points_p[i].y / (Math.tan(fov/2)*points[i].z);
+				points_p[i].x = points_p[i].x / (Math.tan(fov/2)*(points[i].z + 6));
+				points_p[i].y = points_p[i].y / (Math.tan(fov/2)*(points[i].z + 6));
 				//view.drawPoint(new Point2D(points[i].x, tmp.y), c);
-				points[i].x += 0.01;
-				points[i].y += 0.01;
-				//points[i].z += 0.01;
+				//rotate(points[i]);
+				//points[i].x += 0.01;
 				//p.x += 0.01;
 				//p.y += 0.01;
 				//p.z += 0.01;
@@ -61,5 +69,13 @@ public class Test {
 			view.nextFrame();
 		}
 		System.out.println("finished");
+	}
+
+	static void rotate(Point3D p) {
+		Point3D t = new Point3D(p.x, p.y, p.z);
+		p.x = t.x * matrix[0][0] + t.y * matrix[0][1] + t.z * matrix[0][2];
+		p.y = t.x * matrix[1][0] + t.y * matrix[1][1] + t.z * matrix[1][2];
+		p.z = t.x * matrix[2][0] + t.y * matrix[2][1] + t.z * matrix[2][2];
+		//System.out.println("" + p.x);
 	}
 }
